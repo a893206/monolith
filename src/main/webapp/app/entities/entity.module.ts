1ca902142e26431f356f345054a796815f88fd71
CR <931009686@qq.com>
2021-02-01 23:31:41 +0800

    jhipster import-jdl jdl/human_resources.jdl

--- src/main/webapp/app/entities/entity.module.ts
+++ src/main/webapp/app/entities/entity.module.ts
@@ -8,6 +8,38 @@ import { RouterModule } from '@angular/router';
         path: 'phone',
         loadChildren: () => import('./phone/phone.module').then(m => m.MonolithPhoneModule),
       },
+      {
+        path: 'region',
+        loadChildren: () => import('./region/region.module').then(m => m.MonolithRegionModule),
+      },
+      {
+        path: 'country',
+        loadChildren: () => import('./country/country.module').then(m => m.MonolithCountryModule),
+      },
+      {
+        path: 'location',
+        loadChildren: () => import('./location/location.module').then(m => m.MonolithLocationModule),
+      },
+      {
+        path: 'department',
+        loadChildren: () => import('./department/department.module').then(m => m.MonolithDepartmentModule),
+      },
+      {
+        path: 'task',
+        loadChildren: () => import('./task/task.module').then(m => m.MonolithTaskModule),
+      },
+      {
+        path: 'employee',
+        loadChildren: () => import('./employee/employee.module').then(m => m.MonolithEmployeeModule),
+      },
+      {
+        path: 'job',
+        loadChildren: () => import('./job/job.module').then(m => m.MonolithJobModule),
+      },
+      {
+        path: 'job-history',
+        loadChildren: () => import('./job-history/job-history.module').then(m => m.MonolithJobHistoryModule),
+      },
       /* jhipster-needle-add-entity-route - JHipster will add entity modules routes here */
     ]),
   ],
